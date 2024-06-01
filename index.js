830c771fa1ff6b9e157ee02703cf5a0033ed94b5
Mh-Monzil <monzil246@gmail.com>
2024-06-01 18:29:28 +0600

    user validation check with email and password

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 5000;
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6yhdqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,58 +26,86 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const userCollection = client.db("scholarDB").collection("users");
-
+    const usersCollection = client.db("scholarDB").collection("users");
 
     // jwt generate
     app.post("/jwt", async (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: "365d",
-        });
-        res
-          .cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-          })
-          .send({ success: true });
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
       });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
-
-       //clear token on logout
+    //clear token on logout
     app.get("/logout", (req, res) => {
-        res
-          .clearCookie("token", {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-            maxAge: 0,
-          })
-          .send({ success: true });
-      });
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
 
-
-      //save user 
-      app.post('/users', async (req, res) => {
+    //save user
+    app.put("/users", async (req, res) => {
         const user = req.body;
-        console.log(user);
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-      })
+      const email = user.email;
+      const query = { email: email };
+      //check if user already exists in db
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) return res.send(isExist);
 
-      //get all user
-      app.get('/users', async (res,req) => {
-        const result = await userCollection.find().toArray();
-        res.send(result);
-      } )
+      const options = {upsert: true};
+      const updateDoc = {
+        $set: {
+            ...user
+        }
+      }
+      const result = await usersCollection.updateOne(query,updateDoc, options);
+      res.send(result);
+    });
+
+    //get all user
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    //email and password check
+    app.post('/user-validation', async (req, res) => {
+        const {email, password} = req.body;
+        // const password = req.body.password;
+        const queryEmail = {email:email};
+        const emailExists = await usersCollection.findOne(queryEmail);
+        if(emailExists){
+            if(emailExists.password !== password){
+                return res.send("Password is incorrect")
+            }
+        }
+        return res.send("User not found")
+    })
+
+
+
+
 
 
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -90,11 +113,8 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Scholarship is available");
+});
 
-
-
-app.get('/', (req, res) => {
-    res.send('Scholarship is available')
-})
-
-app.listen(port, console.log('Scholarship is running'))
+app.listen(port, console.log("Scholarship is running"));
